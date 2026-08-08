9e6457305e3b4d7cfc607d5191ead6ed87cbe29d
186060 <smgeppert@gmx.net>
2026-08-08 17:48:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Hier den aktuellen Google-Apps-Script-Link deiner Straßenfest-App eintragen.
 window.STRASSENFEST_CONFIG = {
-  APP_URL: "https://DEINE-APP-URL"
+  APP_URL: "https://script.google.com/macros/s/AKfycby6-Knj4BS2gJ1ZdSZwKvD3XM7NIkEGIFjYIGmsksLRbh1kDZZlYQi4-Ck5y6SBfRHBrQ/exec"
 };
