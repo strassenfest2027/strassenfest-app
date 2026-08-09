1197cf067acb662910a53c6efcceb686e9382b44
strassenfest2027 <smgeppert@gmx.net>
2026-08-09 12:44:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// Nur diese Datei musst du normalerweise für die Verbindung zur Google-Apps-Script-App ändern.
+// Hier den aktuellen Google-Apps-Script-Link deiner Straßenfest-App eintragen.
 window.STRASSENFEST_CONFIG = {
-  APP_URL: "https://strassenfest2027.github.io/strassenfest-app/",
+  APP_URL: "https://script.google.com/macros/s/AKfycby6-Knj4BS2gJ1ZdSZwKvD3XM7NIkEGIFjYIGmsksLRbh1kDZZlYQi4-Ck5y6SBfRHBrQ/exec",
   VERSION: "8.1"
 };
