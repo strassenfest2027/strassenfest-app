0707404e1ac94053e9fd24a081ed9a811ccf2767
strassenfest2027 <smgeppert@gmx.net>
2026-08-09 12:54:40 +0200

    Delete js directory

--- js/pwa.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (!("serviceWorker" in navigator)) return;
-
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("./sw.js").catch(function () {
-      // Die App funktioniert auch ohne Service Worker.
-    });
-  });
-})();
